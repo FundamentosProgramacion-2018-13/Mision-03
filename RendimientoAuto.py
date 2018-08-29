36216f245035b385fe7de2545670331c6f56fdd8
A01376466 <42380929+A01376466@users.noreply.github.com>
2018-08-29 14:35:12 -0500

    Update RendimientoAuto.py

--- RendimientoAuto.py
+++ RendimientoAuto.py
@@ -2,24 +2,26 @@
 # Descripción: Este programa calcula el rendimiento de un auto.
 
 
+#Esta función divide los kilometros entre los litros de gasolina para sacar el rendimiento en km/l.
 def metroLitro(kilometros, gasolina):
     rendimientoKmts = kilometros/gasolina
 
     return rendimientoKmts
 
 
+#Esta función manda a llamar a la función anterior para usar el rendimiento en km/l y lo multiplica para sacar la conversión y calcularlo en mi/gal.
 def millaGalon(kilometros, gasolina):
     rendimientoMillas = (metroLitro(kilometros, gasolina))*(1/(1.6093*0.264))
 
     return rendimientoMillas
 
-
+#Esta función divide los kilómetros por recorrer entre el rendimiento en kilómetros para calcular los litros requeridos.
 def necesitaLitro(kilometros, gasolina, kmPorRecorrer):
     necesita = kmPorRecorrer/(metroLitro(kilometros, gasolina))
 
     return necesita
 
-
+#Esta es la función principal para recibir los valores del usuario y llamar al resto de las funciones.
 def main():
     km = float(input("Teclea el número de km recorridos: "))
     gas = float(input("Teclea el número de litros de gasolina usados: "))
