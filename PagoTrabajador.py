46724fb89461cbb714db450f0aa8f7b4efebe547
IrmaGC <42380949+IrmaGC@users.noreply.github.com>
2018-09-01 22:06:53 -0500

    Add files via upload

--- PagoTrabajador.py
+++ PagoTrabajador.py
@@ -5,21 +5,21 @@
 def calcularPagoHN(hNoramles, sueldo):  #HN=horas normales
     pagoHN= hNoramles*sueldo
     return pagoHN
-def calcularPagoHE (hExtras, sueldo):
+def calcularPagoHE (hExtras, sueldo):   #HN=horas extras
     pagoHE= sueldo*1.85*hExtras
     return pagoHE
+def calcularPagoTotal(horasE, horasN, pagoxhora):
+    total=calcularPagoHE(horasE,pagoxhora)+calcularPagoHN(horasN,pagoxhora)
+    return (total)
 
 def main ():
     horasN=int(input("Teclea las horas normales trabajadas: "))
     horasE = int(input("Teclea las horas extras trabajadas: "))
     pagoxhora = int(input("Teclea el pago por hora: "))
-    NumHNormales = calcularPagoHN(horasN,pagoxhora)
-    NumhExtras = calcularPagoHE(horasE,pagoxhora)
-    total = NumhExtras+NumHNormales
     print("""
     Pago normal: $ %.2F
     Pago extra: $ %.2f
     ---------------------
-    Pago total: $ %.2f""" % (NumHNormales,NumhExtras,total))
+    Pago total: $ %.2f""" % (calcularPagoHN(horasN,pagoxhora),calcularPagoHE(horasE, pagoxhora),calcularPagoTotal(horasE,horasN,pagoxhora)))
 
 main ()
