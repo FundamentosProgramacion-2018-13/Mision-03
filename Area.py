532245e3d703e7ecd8c62baabe5de7e5fabeb010
Alex Leyva <42352939+A01747078@users.noreply.github.com>
2018-09-03 13:22:52 -0500

    Update Area.py

--- Area.py
+++ Area.py
@@ -3,23 +3,23 @@
 
 #Permite calcular el área de la figura
 def area(baseMayor, baseMenor, altura):
-    area = ((baseMayor + baseMenor)/2) * altura
-    return area
+    areaFigura = ((baseMayor + baseMenor)/2) * altura
+    return areaFigura
 
 #Permite calcular el perimetro de la figura
 def perimetro(baseMayor, baseMenor, altura):
     ladoIndividual = ((baseMayor - baseMenor) / 2)
     lado = (ladoIndividual ** 2 + altura ** 2) ** 0.5
-    perimetro = baseMenor + baseMayor + 2*lado
-    return perimetro
+    perimetroFigura = baseMenor + baseMayor + 2*lado
+    return perimetroFigura
 
 #Pregunta los datos y llama a las funciones anteriores
 def main():
     baseMayor = int(input("Escribe la longitud de la base mayor: "))
     baseMenor = int(input("Escribe la longitud de la base menor: "))
     altura = int(input("Escribe la altura: "))
-    are = area(baseMayor, baseMenor, altura)
-    per = perimetro(baseMenor, baseMayor, altura)
+    areaFigura = area(baseMayor, baseMenor, altura)
+    perimetroFigura = perimetro(baseMenor, baseMayor, altura)
     print("Área: %.2f" % area)
     print("Perímetro: %.2f" % perimetro)
 
