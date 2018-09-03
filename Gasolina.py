87bf157f7e25d1c4447d3c51feff0a645b663fc4
Alex Leyva <42352939+A01747078@users.noreply.github.com>
2018-09-03 13:20:13 -0500

    Update Gasolina.py

--- Gasolina.py
+++ Gasolina.py
@@ -3,28 +3,28 @@
 
 #Permite calcular el rendimiento en kilometros por litro
 def rendimientolkm(kilometros, litros):
-    lkm = (kilometros / litros)
-    return lkm
+    litrosKilometros = (kilometros / litros)
+    return litrosKilometros
 
 #Permite calcular el rendimiento en millas por galón
 def rendimientogalmi(kilometros, litros):
-    galmi = (kilometros / (litros * 1.6093 * 0.264))
-    return galmi
+    galonesmillas = (kilometros / (litros * 1.6093 * 0.264))
+    return galonesmillas
 
 #Pregunta los datos y llama a las funciones anteriores
 def main():
     kilometros = int(input("Teclea el número de km recorridos: "))
     litros = int(input("Teclea el número de litros de gasolina usados: "))
-    lkm = rendimientolkm(kilometros,litros)
-    galmi = rendimientogalmi(kilometros, litros)
+    litroskilometros = rendimientolkm(kilometros,litros)
+    galonesmillas = rendimientogalmi(kilometros, litros)
     print("   ")
     print("Si recorres", kilometros, "kms con", litros, "de gasolina, el rendimiento es: ")
-    print("%.2f km/l" % lkm)
-    print("%.2f mi/gal" % galmi)
+    print("%.2f km/l" % litrosKilometros)
+    print("%.2f mi/gal" % galonesmillas)
     print("   ")
     kmrecorrer = (int(input("¿Cuántos kilómetros vas a recorrer? ")))
     print("   ")
-    ren = kmrecorrer / lkm
-    print("Para recorrer", kmrecorrer, "km. necesitas %.2f" % ren, "litros de gasolina")
+    rendimiento = kmrecorrer / lkm
+    print("Para recorrer", kmrecorrer, "km. necesitas %.2f" % rendimiento, "litros de gasolina")
 
 main()
