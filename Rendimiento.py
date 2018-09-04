0f189f911f282ac85fed8fa49f11e57b3af47f90
DanCordova <42380930+DanCordova@users.noreply.github.com>
2018-09-03 21:34:11 -0500

    Update Rendimiento.py

--- Rendimiento.py
+++ Rendimiento.py
@@ -22,11 +22,14 @@ def main():
     rendimientoKmL = calcularRendimientoKm (kmRecorridos, litrosGasolinaUsados)
     rendimientoMiG = calcularRendimientoMi (kmRecorridos, litrosGasolinaUsados)
 
+    print("")
     print("Si recorres %d kms con %d litros de gasolina, el rendimiento es: " %(kmRecorridos, litrosGasolinaUsados))
     print("%.2f km/l" %(rendimientoKmL))
     print("%.2f mi/gal" %(rendimientoMiG))
+    print("")
     kmPorRecorrer = int(input("¿Cuántos kilómetros vas a recorrer?"))
     litrosAUsar = calcularLitrosNecesarios(kmRecorridos, litrosGasolinaUsados, kmPorRecorrer)
+    print("")
     print("Para recorrer %d km. necesitas %.2f litros de gasolina" %(kmPorRecorrer, litrosAUsar))
 
 main()
