ddcd807975410e030a53503439cffcd6320b0884
LuisaArellano <42380922+LuisaArellano@users.noreply.github.com>
2018-09-03 21:08:45 -0500

    Add files via upload

--- AsientosEstadio.py
+++ AsientosEstadio.py
@@ -1,26 +1,19 @@
 #Autor: Luisa Fernada Arellano Alvarado A01377974
 
 
+def calcularPago(AsientoA,AsientoB, AsientoC):
+    totalPago = AsientoA*925 + AsientoB*775 + AsientoC*360
+    return totalPago
 
-
-
-def calcularPago(numeroBoletosA,numeroBoletosB, numeroBoletosC):
-    Pago = numeroAsientosA * 925
-    Pago = Pago + numeroBoletosB * 775 + numeroBoletosC * 360
-    return Pago
-
-
-
-  
 
 
 def main():
-    numeroBoletosA = int(input("Teclea número de boletos de clase A:"))
-    numeroBoletosB = int(input("Teclea número de boletos de clase B:"))
-    numeroBoletosC = int(input("Teclea número de boletos de clase C:"))
-    total = calcularPago
+    numeroBoletosA = int(input("Teclea número de boletos de clase A: "))
+    numeroBoletosB = int(input("Teclea número de boletos de clase B: "))
+    numeroBoletosC = int(input("Teclea número de boletos de clase C: "))
+    totalPago = calcularPago(numeroBoletosA,numeroBoletosB,numeroBoletosC)
  
-    
+    print("El costo total es: $%.2d"%(totalPago))
 
 
 # Llamamos a la función main
